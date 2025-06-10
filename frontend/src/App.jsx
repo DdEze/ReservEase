e77471a25ed68772f1a1a9fec358686bde8100e7
Ezequiel <ezequielensenada@yahoo.com.ar>
2025-06-10 00:18:41 -0300

    Agregando routas a pages y components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,42 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
+import PrivateRoute from './components/PrivateRoute';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/spaces"
+          element={
+            <PrivateRoute>
+              <div>Página de Espacios</div>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/reservations"
+          element={
+            <PrivateRoute>
+              <div>Página de Reservas</div>
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
