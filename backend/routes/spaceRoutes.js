65004c3859fe09ae3af198cc02527a89ab9bc421
Ezequiel <ezequielensenada@yahoo.com.ar>
2025-06-10 23:58:18 -0300

    Update

--- backend/routes/spaceRoutes.js
+++ backend/routes/spaceRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const spaceController = require('../controllers/spaceController');
+const { getSpaces, createSpace } = require('../controllers/spaceController');
 const auth = require('../middleware/authMiddleware');
 const requireAdmin = require('../middleware/requireAdmin');
 const { spaceValidator } = require('../validators/spaceValidator');
 const validate = require('../middleware/validationMiddleware');
 
-router.get('/', spaceController.getSpaces);
-router.post('/', auth, requireAdmin, spaceValidator, validate, spaceController.createSpace);
+router.get('/', getSpaces);
+router.post('/', auth, requireAdmin, spaceValidator, validate, createSpace);
 
 module.exports = router;
