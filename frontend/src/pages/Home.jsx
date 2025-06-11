1977dcdbe7a9b1c96a8215a70b02ded1bc741748
Ezequiel <ezequielensenada@yahoo.com.ar>
2025-06-10 23:59:38 -0300

    Home Update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,35 +1,20 @@
 import React from 'react';
 import { Container, Typography, Box, Button } from '@mui/material';
 import { useAuth } from '../context/AuthContext';
-import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
   const { user, setUser } = useAuth();
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    setUser(null);
-    navigate('/login');
-  };
 
   return (
     <Container maxWidth="md">
       <Box mt={5} textAlign="center">
         <Typography variant="h4" gutterBottom>
-          ¡Bienvenido{user?.surname ? `, ${user.name} ${user.surname}` : ''}!
+          ¡Bienvenido{user?.surname ? `, ${user.surname} ${user.name}` : ''}!
         </Typography>
         <Typography variant="body1" gutterBottom>
-          Este es tu panel de control. Pronto vas a poder ver tus reservas o crear nuevas.
+           Bienvenido al sistema de reservas. Aquí podés buscar espacios disponibles, ver tus reservas activas o cancelar alguna si lo necesitás. 
+  Si tenés alguna duda, contactanos.
         </Typography>
-
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={handleLogout}
-          sx={{ mt: 4 }}
-        >
-          Cerrar sesión
-        </Button>
       </Box>
     </Container>
   );
