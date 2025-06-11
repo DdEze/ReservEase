36ed3093b174dd334d8e6a4833a69bdf6f40e466
Ezequiel <ezequielensenada@yahoo.com.ar>
2025-06-10 23:57:54 -0300

    Cancelar reserva creada

--- backend/routes/reservationRoutes.js
+++ backend/routes/reservationRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const reservationController = require('../controllers/reservationController');
+const { createReservation, getMyReservations, deleteReservation } = require('../controllers/reservationController');
 const auth = require('../middleware/authMiddleware');
 const { reservationValidator } = require('../validators/reservationValidator');
 const validate = require('../middleware/validationMiddleware');
 
-router.post('/', auth, reservationValidator, validate, reservationController.createReservation);
-router.get('/mine', auth, reservationController.getMyReservations);
+router.post('/', auth, reservationValidator, validate, createReservation);
+router.get('/mine', auth, getMyReservations);
+router.delete('/:id', auth, deleteReservation);
 
 module.exports = router;
