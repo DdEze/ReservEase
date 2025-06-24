9b272412562abfccbfa57117e3e46d0b3e0dc8c2
Ezequiel <ezequielensenada@yahoo.com.ar>
2025-06-23 23:23:25 -0300

    Se agregaron nuevas rutas

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const { register, login, getAllUsers, updateUserRole } = require('../controllers/authController');
 const { registerValidator } = require('../validators/authValidator');
 const validate = require('../middleware/validationMiddleware');
 const authMiddleware = require('../middleware/authMiddleware');
@@ -9,6 +9,8 @@ const User = require('../models/User');
 
 router.post('/register', registerValidator, validate, register);
 router.post('/login', login);
+router.get('/', authMiddleware, requireAdmin, getAllUsers);
+router.put('/:id/role', authMiddleware, requireAdmin, updateUserRole);
 router.delete('/delete/:email', authMiddleware, requireAdmin, async (req, res) => {
   try {
     const deleted = await User.findOneAndDelete({ email: req.params.email });
